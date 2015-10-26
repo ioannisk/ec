25719f816f3d17f0cf4b65dcad97e9dfc39ca83d
ioannisk <john.karamanlakis@gmail.com>
2015-10-26 09:43:59 +0000

    task9

--- t9-reduce.py
+++ t9-reduce.py
@@ -7,14 +7,16 @@ lowest_score = sys.maxint
 
 for line in sys.stdin:          # For ever line in the input from stdin
     line = line.strip()         # Remove trailing characters
-    name, score = line.split("\t", 1)
-    score = float(score)   
-    if score < lowest_score:
-    	del lowest_names[:]
-    	lowest_score = score
-    	lowest_names.append(name)
-    elif score == lowest_score:
-    	lowest_score.append(name)
+    tokens = line.split("\t", 1)
+    if len(tokens) == 2:
+        name = tokens[0]
+        score = float(tokens[1])   
+        if score < lowest_score:
+        	del lowest_names[:]
+        	lowest_score = score
+        	lowest_names.append(name)
+        elif score == lowest_score:
+        	lowest_score.append(name)
 
 for name in lowest_names:
 	print ("{0}\t{1:.2f}".format(name, lowest_score))
