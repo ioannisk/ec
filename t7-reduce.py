8d9b66ae5b36ca5abaaa7470a8fe09eb5821c408
ioannisk <john.karamanlakis@gmail.com>
2015-10-26 11:44:41 +0000

    mm

--- t7-reduce.py
+++ t7-reduce.py
@@ -10,10 +10,10 @@ trans_row = []
 
 def output_row(row, cell_list):
     cell_list = sorted(cell_list, key=lambda tup: tup[0])
-    sys.stdout.write("{0}: ".format(row))
+    sys.stdout.write("{0}:".format(row))
     for cell in cell_list:
         sys.stdout.write("{0} ".format(cell[1]))
-    print 
+    print
 
 for line in sys.stdin:          # For ever line in the input from stdin
     line = line.strip()         # Remove trailing characters
@@ -23,9 +23,9 @@ for line in sys.stdin:          # For ever line in the input from stdin
     # Remember that Hadoop sorts map output by key reducer takes these keys sorted
     if (prev_col != "") and (prev_col != col):
         output_row(prev_col, trans_row)
-        del trans_row[:]    #flush cell list 
+        del trans_row[:]    #flush cell list
     trans_row.append( (int(row), cell) )
     prev_col = col
-    
+
 if prev_col == col:  # Don't forget the last key/value pair
     output_row(prev_col, trans_row)
