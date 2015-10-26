4b5561a6890dca92bc0d36e4790de296a2053af0
ioannisk <john.karamanlakis@gmail.com>
2015-10-26 09:30:48 +0000

    fixing bugs

--- t9-map.py
+++ t9-map.py
@@ -7,7 +7,8 @@ for line in sys.stdin:
 	line = line.strip()
 	line = line.split(" ", 2)
 	name = line[0]
-	scores = re.compile('\d+').findall(line[2])
-	if len(scores) > 4:
-		avg = sum([ int(s) for s in scores])/float(len(scores))
-		print("{0}\t{1:.2f}".format(name, avg))
+	if len(line) == 3:
+		scores = re.compile('\d+').findall(line[2])
+		if len(scores) > 4:
+			avg = sum([ int(s) for s in scores])/float(len(scores))
+			print("{0}\t{1:.2f}".format(name, avg))
