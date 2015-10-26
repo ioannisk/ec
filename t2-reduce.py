73e022f2f41a2c8b1c38d7498858b8d195357396
ioannisk <john.karamanlakis@gmail.com>
2015-10-26 10:34:27 +0000

    bugs

--- t2-reduce.py
+++ t2-reduce.py
@@ -5,7 +5,7 @@ import sys
 prev_line = ""
 
 for line in sys.stdin:          # For ever line in the input from stdin
-	line = line.strip()
+    line = line.strip()
     if prev_line != line:
         print line
         prev_line = line
--- t4-reduce.py
+++ t4-reduce.py
@@ -7,18 +7,20 @@ value_total = 0
 bigram=""
 
 for line in sys.stdin:          # For ever line in the input from stdin
-    line = line.strip()         # Remove trailing characters
-    bigram, value = line.split("\t", 1)
-    value = int(value)
-    # Remember that Hadoop sorts map output by key reducer takes these keys sorted
-    if prev_bigram == bigram:
-        value_total += value
-    else:
-        if prev_bigram:  # write result to stdout
-            print("{0}\t{1}".format(prev_bigram, value_total))
-            
-        value_total = value
-        prev_bigram = bigram
+    try:
+        line = line.strip()         # Remove trailing characters
+        bigram, value = line.split("\t", 1)
+        value = int(value)
+        # Remember that Hadoop sorts map output by key reducer takes these keys sorted
+        if prev_bigram == bigram:
+            value_total += value
+        else:
+            if prev_bigram:  # write result to stdout
+                print("{0}\t{1}".format(prev_bigram, value_total))
+            value_total = value
+            prev_bigram = bigram
+    except:
+        continue
 
 if prev_bigram == bigram:  # Don't forget the last key/value pair
     print("{0}\t{1}".format(prev_bigram, value_total))
