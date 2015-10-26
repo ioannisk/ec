c145bcb4990923691cd8b18e89f03a6c309a0e2c
ioannisk <john.karamanlakis@gmail.com>
2015-10-26 11:34:02 +0000

    task 7

--- t7-map.py
+++ t7-map.py
@@ -6,6 +6,7 @@ import sys
 for line in sys.stdin:                  # input from standard input
     line = line.strip()                 # remove whitespaces
     row, tokens = line.split("\t", 1)               # split the line into tokens
+    tokens = tokens.split()
     for col in range(0, len(tokens)):                # write the results to standard output
         # use col as the key, pass row as part of the value so reducer can sort it
         print("{0}\t{1} {2}".format(col, row, tokens[col]))
