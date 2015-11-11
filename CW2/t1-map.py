bdaee47538007bdf6cc0936214b745fd978289a6
ioannisk <john.karamanlakis@gmail.com>
2015-11-11 10:17:21 +0000

    changing print statements

--- CW2/t1-map.py
+++ CW2/t1-map.py
@@ -15,6 +15,8 @@ for line in sys.stdin:                  # input from standard input
 
     # find from which file we are reading
     file_name = os.environ["mapreduce_map_input_file"]
+    # format string such that we dont print the whole file path
+    file_name = file_name.split('/')[-1:]
 
     line = line.strip()                 # remove whitespaces
     tokens = parse(line)               # split the line into tokens
