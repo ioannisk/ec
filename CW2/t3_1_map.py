b0b0e3d1bb1d89777858b38c4dde764ff2bbb0b8
ioannisk <john.karamanlakis@gmail.com>
2015-11-28 17:22:39 +0000

    ignore malformed input

--- CW2/t3_1_map.py
+++ CW2/t3_1_map.py
@@ -6,13 +6,15 @@ import sys
 for line in sys.stdin:
     line = line.strip()
     line = line.split()
-    # we parse the input
-    host = line[0]
-    timestamp = line[3] + line[4]
-    # req_method+req_dir+req_http concatenated are the whole request
-    # req_method = line[5]
-    req_web_server  = line[6]
-    # req_http = line[7]
-    # reply = line[8]
-    # bytes = line[9]
-    print('{0}\t{1}'.format(req_web_server, 1))
+    # if the lenght of the split is not 10 that means we have malformed input, we can ignore it
+    if len(line) == 10:
+        # we parse the input
+        host = line[0]
+        timestamp = line[3] + line[4]
+        # req_method+req_dir+req_http concatenated are the whole request
+        req_method = line[5]
+        req_web_server  = line[6]
+        req_http = line[7]
+        reply = line[8]
+        bytes = line[9]
+        print('{0}\t{1}'.format(req_web_server, 1))
