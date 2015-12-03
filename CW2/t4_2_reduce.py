bdfd1d8ce167bd8227eb2350acd42b8b65529f34
ioannisk <john.karamanlakis@gmail.com>
2015-12-03 19:57:43 +0000

    m

---
+++ CW2/t4_2_reduce.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import sys
+import re
+import datetime
+
+prev_key = ""
+key = ""
+value_total = 0
+questions_list = []
+
+max_key =""
+max_value = 0
+max_question_list =[]
+
+def print_out():
+    global max_key
+    global max_question_list
+    global max_value_total
+    sys.stdout.write(key+"\t->\t")
+    for i in max_question_list:
+        sys.stdout.write("PostId, ")
+    sys.stdout.write("\n")
+    sys.stdout.write(str(max_value)+"\t->\t")
+    for i in max_question_list:
+        sys.stdout.write("{0}, ".format(i))
+
+for line in sys.stdin:
+    line = line.strip()
+    key, Id ,value = line.split("\t", 2)
+    value = int(value)
+    if prev_key == key:
+        value_total += value
+        questions_list.append(Id)
+    else:
+        if prev_key:
+            if max_value <= value_total:
+                max_value = value_total
+                max_key = key
+                max_question_list = questions_list
+                questions_list = []
+
+        value_total = value
+        prev_key = key
+        questions_list.append(Id)
+
+if prev_key == key:
+    if max_value <= value_total:
+        max_value = value_total
+        max_key = key
+        max_question_list = questions_list
+        questions_list = []
+
+print_out()
