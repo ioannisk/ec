cf8144083ae1d0297d65ae43938da3afdf7bd3d1
ioannisk <john.karamanlakis@gmail.com>
2015-12-04 12:09:52 +0000

    m

--- CW2/t4_3_map.py
+++ CW2/t4_3_map.py
@@ -10,12 +10,24 @@ for line in sys.stdin:
     # if the lenght of the split is not 10 that means we have malformed input, we can ignore it
     try:
         m = ET.fromstring(line)
-        user = m.attrib['OwnerUserId']
-        parent_question = m.attrib['ParentId']
-        # view_count = m.attrib['ViewCount']
-        # post_type 1 for question 2 for answer
         post_type = m.attrib['PostTypeId']
-        if post_type=='2':
-            print('{0}\t{1}\t{2}'.format(user, parent_question, 1))
+        if post_type == '2':
+            try:
+                user = m.attrib['OwnerUserId']
+                parent_q = m.attrib['ParentId']
+                # ANSWER ID
+                Id = m.attrib['Id']
+                print ("{0}\t{1}\t{2}\t{3}".format(Id, post_type, parent_q, user))
+            except:
+                continue
+        else:
+            try:
+                user = m.attrib['OwnerUserId']
+                acc_ans = m.attrib['AcceptedAnswerId']
+                # QUESTION ID
+                Id = m.attrib['Id']
+                print ("{0}\t{1}\t{2}\t{3}".format(acc_ans, post_type, Id, user))
+            except:
+                continue
     except:
         continue
